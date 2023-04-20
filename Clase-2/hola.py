c13899dc9403b9ed95656cc1a0e25523d270ad26
galmarcano <genesismarc@gmail.com>
2023-04-20 18:28:57 -0400

    clase-2

---
+++ Clase-2/hola.py
@@ -0,0 +1,15 @@
+print("Hola Génesis!")
+print("Segunda línea")
+
+nombre = "Génesis Marcano"
+colorFavorito = "Azul"
+edad = 29.9
+diasParaCumple = 5
+ansiosaPorCumple = False
+
+print(nombre, colorFavorito, edad, diasParaCumple, ansiosaPorCumple)
+
+numero_1 = 1
+numero_2 = 4
+resultado = numero_1+numero_2-numero_1
+print (resultado)
